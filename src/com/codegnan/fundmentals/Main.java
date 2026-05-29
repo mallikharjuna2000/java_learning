40b2ee5d97b1da7d5e98c1bd454a2756068235e5
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-05-29 12:34:26 +0530

    feat:Account Based senario using operators

---
+++ src/com/codegnan/fundmentals/Main.java
@@ -0,0 +1,19 @@
+package com.codegnan.fundmentals;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int balance=scanner.nextInt();
+		int deposit=scanner.nextInt();
+		balance+=deposit;
+		boolean isEvenAndGreater1000 =(balance>1000)&&(balance%2==0);
+		System.out.println(balance);
+		System.out.println(isEvenAndGreater1000);
+		scanner.close();
+
+	}
+
+}
