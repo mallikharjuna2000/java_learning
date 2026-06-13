04a16916975b85308f83beafef5f988002071ccd
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-06-13 12:11:16 +0530

    feat:Day-18 inheritance and super() examples

---
+++ src/com/codegnan/oopprogramming/My_Calculation.java
@@ -0,0 +1,12 @@
+package com.codegnan.oopprogramming;
+
+public class My_Calculation extends Calculation {
+
+	public My_Calculation(int num1, int num2) {
+		super(num1, num2);
+	}
+
+	public int multiplication() {
+		return num1 * num2;
+	}
+}
