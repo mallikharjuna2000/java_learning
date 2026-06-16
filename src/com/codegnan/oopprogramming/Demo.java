409f01677f9d1db483c57e73a3d4afbb17eddb5d
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-06-16 15:24:55 +0530

    feat:String .equals() and StringBuilder .equals() diff

--- src/com/codegnan/oopprogramming/Demo.java
+++ src/com/codegnan/oopprogramming/Demo.java
@@ -3,14 +3,16 @@ package com.codegnan.oopprogramming;
 public class Demo {
 
 	public static void main(String[] args) {
-		//immultability
-		String s = new String("codegnan");
-		s.concat("solutions");
-		System.out.println(s);
-        //mutability
-		StringBuilder sb=new StringBuilder("codegnan");
-		sb.append("solutions");
-		System.out.println(sb);
+		String s1 = new String("Codegnan");
+		String s2 = new String("Codegnan");
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+		
+		StringBuilder sb1 = new StringBuilder("Codegnan");
+		StringBuilder sb2 = new StringBuilder("Codegnan");
+		System.out.println(sb1 == sb2);
+		System.out.println(sb1.equals(sb2));
+	
 	}
 
 }
